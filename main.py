3eead519b27008901990a035335ed58b4ece2861
ArkJzzz <arkjzzz@gmail.com>
2021-07-08 13:12:56 +0300

    более точные названия переменных

--- main.py
+++ main.py
@@ -7,49 +7,52 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
+def main():
 
-template = env.get_template('template.html')
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
 
-winery_foundation = datetime.date(year=1920, month=1, day=1)
-today = datetime.date.today()
-winery_age = today.year - winery_foundation.year
+    template = env.get_template('template.html')
 
-excel_data_df = pandas.read_excel(
-            io='wine.xlsx',
-            sheet_name='Лист1',
-            na_values='nan',
-            keep_default_na=False,
-        )
-bottles = excel_data_df.to_dict('records')
-categories = collections.defaultdict(list)
+    winery_foundation_year = datetime.date(year=1920, month=1, day=1)
+    today = datetime.date.today()
+    winery_age = today.year - winery_foundation.year
 
-for bottle in bottles:
-    categories[bottle['Категория']].append(
-            {
-                'title': bottle['Название'],
-                'variety': bottle['Сорт'],
-                'price': bottle['Цена'],
-                'image': bottle['Картинка'],
-                'promotion': bottle['Акция']
-            }
-        )
+    excel_data_df = pandas.read_excel(
+                io='wine.xlsx',
+                sheet_name='Лист1',
+                na_values='nan',
+                keep_default_na=False,
+            )
+    bottles = excel_data_df.to_dict('records')
+    assortment = collections.defaultdict(list)
 
-categories = collections.OrderedDict(sorted(categories.items()))
+    for bottle in bottles:
+        assortment[bottle['Категория']].append(
+                {
+                    'title': bottle['Название'],
+                    'variety': bottle['Сорт'],
+                    'price': bottle['Цена'],
+                    'image': bottle['Картинка'],
+                    'promotion': bottle['Акция']
+                }
+            )
 
-pprint(categories)
+    assortment = collections.OrderedDict(sorted(assortment.items()))
+
+    rendered_page = template.render(
+        winery_age=winery_age,
+        assortment=assortment,
+    )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
 
 
-rendered_page = template.render(
-    winery_age=winery_age,
-    categories=categories,
-)
-
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
-
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+if __name__ == '__main__':
+    main()
